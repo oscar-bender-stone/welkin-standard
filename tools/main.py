9180213e8fff5bc7b97ce760146421bf7a0caf24
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-28 20:04:54 -0600

    Initial commit

---
+++ tools/main.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
+# SPDX-License-Identifier: MIT
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
