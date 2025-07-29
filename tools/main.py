d0be865e4e96613ddb17bdfc786a586366ad5137
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-28 21:09:32 -0600

    Cleaned up directories and worked on HTML generation

--- tools/main.py
+++ tools/main.py
@@ -2,8 +2,31 @@
 # SPDX-License-Identifier: MIT
 
 
+def generate_html():
+    import pypandoc
+
+    language_code = "eng"
+
+    pypandoc.convert_file(
+        f"document/{language_code}/*.wiki",
+        "html",
+        outputfile="html/eng/index.html",
+        format="mediawiki",
+        extra_args=[
+            "--template",
+            "tools/template.html",
+            "-V",
+            f"lang={language_code}",
+            "--metadata",
+            "title='The Welkin Standard'",
+            # "--metadata",
+            # "pagetitle='The Welkin Standard'",
+        ],
+    )
+
+
 def main():
-    raise NotImplementedError
+    generate_html()
 
 
 if __name__ == "__main__":
