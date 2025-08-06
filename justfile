bd33761a4351f48bc68df371d12ee99b3b7d8a63
Oscar Bender-Stone <oscarbenderstone@gmail.com>
2025-08-05 18:47:49 -0600

    Switched from ruby dependency to NodeJS for markup

--- justfile
+++ justfile
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: 2025 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
 # SPDX-License-Identifier: MIT
 
-build:
-    ruby tools/markup/main.rb
+html:
+	npm --prefix "tools/markup" run build
+	@echo "Succesfully generated HTML."
 
-lint:
+reuse:
     uv run reuse lint
---
+++ tools/markup/index.js
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2025 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
+// SPDX-License-Identifier: MIT
+
+import Asciidoctor from "@asciidoctor/core";
+import * as fs from "node:fs/promises";
+import * as ejs from "ejs";
+import * as path from "path";
+import * as prettier from "prettier";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const asciidoctor = Asciidoctor();
+
+const language = "eng";
+
+const input_directory = path.join(__dirname, "../../", "document", language);
+const output_directory = path.join(__dirname, "../../", "html", language);
+
+
+var document = asciidoctor.loadFile(path.join(input_directory, "index.mink"));
+const html_body = document.convert();
+
+const template = await fs.readFile(path.join(__dirname, "template.html5.ejs"), {encoding: "utf8"})
+
+var html = ejs.render(template, {html_body: html_body});
+html = await prettier.format(html, { semi: false, parser: "html"})
+
+fs.writeFile(path.join(output_directory, "index.html"), html)
