0b429f7dafacc5ee7449b15ed19dcfcf56fa12c6
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2025-08-19 17:44:49 -0600

    Fixed HTML generation for tables and external files

--- tools/markup/index.js
+++ tools/markup/index.js
@@ -17,8 +17,15 @@ const language = "eng";
 const input_directory = path.join(__dirname, "../../", "document", language);
 const output_directory = path.join(__dirname, "../../", "html", language);
 
-var document = asciidoctor.loadFile(path.join(input_directory, "index.mink"));
-const html_body = document.convert();
+const base_paths = ["index.mink", "syntax.mink"]
+
+var html_body = "";
+var document;
+
+for (const base_path of base_paths) {
+  document = asciidoctor.loadFile(path.join(input_directory, base_path));
+  html_body += document.convert()
+}
 
 const template = await fs.readFile(path.join(__dirname, "template.html5.ejs"), {encoding: "utf8"})
 var html = ejs.render(template, {html_body: html_body});
